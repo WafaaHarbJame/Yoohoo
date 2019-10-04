f730184507a672e8ead7103c5641cf678145abfd
eng wafaa <wafaajame@gmail.com>
2019-10-04 16:32:38 +0200

    Initial commit

---
+++ app/build/generated/source/apt/debug/io/realm/com_verbosetech_yoohoo_models_MyStringRealmProxyInterface.java
@@ -0,0 +1,7 @@
+package io.realm;
+
+
+public interface com_verbosetech_yoohoo_models_MyStringRealmProxyInterface {
+    public String realmGet$string();
+    public void realmSet$string(String value);
+}
